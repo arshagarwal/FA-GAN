b63fd3bd381e82b746e44b311c6d02b4629a9277
Arsh <arshagarwal.18@gmail.com>
2020-10-29 20:28:05 +0530

    fixed typo

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
 # Train
-python main.py --rafd_image_dir ../Face-chq2 --num_iters 30000 --c_dim 2 --batch_size 8 --n_critic 1 --image_size 256 --num_workers 5 --gpus 0,1 
+python main.py --img_dir ../Face-chq2 --num_iters 30000 --c_dim 2 --batch_size 8 --n_critic 1 --image_size 256 --num_workers 5 --gpus 0,1 --lambda_rec 1 
 # Test
-python main.py --rafd_image_dir ../Face-chq2 --num_iters 30000 --c_dim 2 --batch_size 8 --n_critic 1 --image_size 256 --num_workers 5 --gpus 0,1 --mode test --test_iters 30000
+python main.py --img_dir ../Face-chq2 --num_iters 30000 --c_dim 2 --batch_size 8 --n_critic 1 --image_size 256 --num_workers 5 --gpus 0,1 --mode test --test_iters 30000
